d7efaaf5e8fe8ad2720e6d219fbde95b1ad9ca05
tanuja5 <1997.tanuja@gmail.com>
2018-09-29 16:25:49 +0530

    Update computeshape.py

--- computeshape.py
+++ computeshape.py
@@ -5,11 +5,16 @@ while(True):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
     template = cv2.imread('pen0.png',cv2.IMREAD_GRAYSCALE)
-        #w, h = template.shape[::-1]
+    w, h = template.shape[::-1]
         
     res = cv2.matchTemplate(gray,template,cv2.TM_CCOEFF_NORMED)
-    print(np.where(res>=0.8))
-    cv2.imshow('Test',frame)
+    
+    loc=np.where(res >= 0.7)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(frame, pt, (pt[0]+ w, pt[1] + h), (0,255,0), 3)
+        
+    #cv2.imshow('Test',frame)
+    cv2.imshow('Test1', res)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
